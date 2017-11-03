e77568c866ada8006f5b6627cece1c7ef3cf2150
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-11-03 21:22:32 +0300

    Added sortings

---
+++ Lab3. Sorting/gnome sort.py
@@ -0,0 +1,85 @@
+import random
+import time
+
+N = 10000
+
+
+def std_gnome_sort(x):
+    i = 1
+    while i < len(x):
+        if not i or x[i - 1] <= x[i]:
+            i += 1
+        else:
+            x[i], x[i - 1] = x[i - 1], x[i]
+            i -= 1
+    return x
+
+
+def opt_gnome_sort(x):
+    i, j, size = 1, 2, len(x)
+    while i < size:
+        if x[i - 1] <= x[i]:
+            i, j = j, j + 1
+        else:
+            x[i - 1], x[i] = x[i], x[i - 1]
+            i -= 1
+            if i == 0:
+                i, j = j, j + 1
+
+    return x
+
+
+if __name__ == "__main__":
+    a = []
+    b = []
+    c = []
+    for i in range(N):
+        a.append(i)
+        b.append(N - i)
+        c.append(random.randint(-N, N))
+
+    print("NO OPTIMIZED std_shell_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = std_gnome_sort(a)
+    end = time.time()
+    print(na == sorted(a))
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = std_gnome_sort(b)
+    end = time.time()
+    print(nb == sorted(b))
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = std_gnome_sort(c)
+    end = time.time()
+    print(nc == sorted(c))
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n\nOPTIMIZED opt_shell_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = opt_gnome_sort(a)
+    end = time.time()
+    print(na == sorted(a))
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = opt_gnome_sort(b)
+    end = time.time()
+    print(nb == sorted(b))
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = opt_gnome_sort(c)
+    end = time.time()
+    print(nc == sorted(c))
+    print("\nTIME:", (end - st), "  sec")
---
+++ Lab3. Sorting/selection sort.py
@@ -0,0 +1,72 @@
+import random
+import time
+
+N = 1000
+
+
+def std_selection_sort(x):
+    for i in range(len(x)):
+        min_ind = i
+        for j in range(i, len(x)):
+            if x[min_ind] > x[j]:
+                min_ind = j
+        x[i], x[min_ind] = x[min_ind], x[i]
+
+    return x
+
+
+def opt_selection_sort(x):
+    for i, e in enumerate(x):
+        mn = min(range(i, len(x)), key=x.__getitem__)
+        x[i], x[mn] = x[mn], e
+    return x
+
+
+if __name__ == "__main__":
+    a = []
+    b = []
+    c = []
+    for i in range(N):
+        a.append(i)
+        b.append(N - i)
+        c.append(random.randint(-N, N))
+
+    print("NO OPTIMIZED std_shell_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = std_selection_sort(a)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = std_selection_sort(b)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = std_selection_sort(c)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n\nOPTIMIZED opt_shell_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = opt_selection_sort(a)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = opt_selection_sort(b)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = opt_selection_sort(c)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
