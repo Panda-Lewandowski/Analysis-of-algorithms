f571bceb3e255dfe96f34afb4215d78015ec4d09
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-11-08 18:47:16 +0300

    Added improve multi

--- Lab2. MultiMatrix/classic_multi_matrix.py
+++ Lab2. MultiMatrix/classic_multi_matrix.py
@@ -1,11 +1,23 @@
+import numpy as np
+
 def classic_multi(A, B):
     if len(B) != len(A[0]):
         print("Different dimension of the matrics")
         return
-    return [[sum(x * B[i][col] for i, x in enumerate(row)) for col in range(len(B[0]))] for row in A]
+
+    n = len(A)
+    m = len(A[0])
+    t = len(B[0])
+
+    answer = [[0 for i in range(t)] for j in range(n)]
+    for i in range(n):
+        for j in range(m):
+            for k in range(t):
+                answer[i][k] += A[i][j] * B[j][k]
+    return answer
 
 
 if __name__ == "__main__":
     A = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
     B = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7], [4, 5, 6, 7, 8]]
-
+    print(classic_multi(A, B))
--- Lab2. MultiMatrix/tests.py
+++ Lab2. MultiMatrix/tests.py
@@ -1,6 +1,7 @@
 from random import randint
 from time import time
 from classic_multi_matrix import classic_multi
+from imprv_classic_multi_matrix import imprv_classic_multi
 from winograd_multi_matrix import winograd_multi
 
 
@@ -13,10 +14,10 @@ def time_testing():
 
     print("5 X 5\n")
 
-    a = random_matrix(5, 5)
-    b = random_matrix(5, 5)
+    a = random_matrix(20, 20)
+    b = random_matrix(20, 20)
 
-    t1, t2 = 0, 0
+    t1, t2, t3 = 0, 0, 0
 
     for i in range(100):
 
@@ -32,16 +33,23 @@ def time_testing():
 
         t2 += (stop - start)
 
+        start = time()
+        res_imprv = imprv_classic_multi(a, b)
+        stop = time()
+
+        t3 += (stop - start)
+
     print("Classic multiplication of matrix: {0:f} sec\n"
           "Winorgad multiplication of matrix: {1:f} sec\n"
-          "Are result matrix equivalent? {2}".format(t1 * 10, t2 * 10, res_class == res_wino))
+          "Imporove classic multiplication of matrix: {2:f} sec\n"
+          "Are result matrix equivalent? {3}".format(t1 * 10, t2 * 10, t3 * 10, res_class == res_wino))
 
     print("\n\n20 X 20\n")
 
     a = random_matrix(20, 20)
     b = random_matrix(20, 20)
 
-    t1, t2 = 0, 0
+    t1, t2, t3 = 0, 0, 0
 
     for i in range(100):
         start = time()
@@ -55,17 +63,24 @@ def time_testing():
         stop = time()
 
         t2 += (stop - start)
+
+        start = time()
+        res_imprv = imprv_classic_multi(a, b)
+        stop = time()
+
+        t3 += (stop - start)
 
     print("Classic multiplication of matrix: {0:f} sec\n"
           "Winorgad multiplication of matrix: {1:f} sec\n"
-          "Are result matrix equivalent? {2}".format(t1 * 10, t2 * 10, res_class == res_wino))
+          "Imporove classic multiplication of matrix: {2:f} sec\n"
+          "Are result matrix equivalent? {3}".format(t1 * 10, t2 * 10, t3 * 10, res_class == res_wino))
 
     print("\n\n40 X 40\n")
 
-    a = random_matrix(130, 100)
-    b = random_matrix(100, 130)
+    a = random_matrix(130, 130)
+    b = random_matrix(130, 130)
 
-    t1, t2 = 0, 0
+    t1, t2, t3 = 0, 0, 0
 
     for i in range(100):
         start = time()
@@ -80,9 +95,16 @@ def time_testing():
 
         t2 += (stop - start)
 
+        start = time()
+        res_imprv = imprv_classic_multi(a, b)
+        stop = time()
+
+        t3 += (stop - start)
+
     print("Classic multiplication of matrix: {0:f} msec\n"
           "Winorgad multiplication of matrix: {1:f} msec\n"
-          "Are result matrix equivalent? {2}".format(t1 * 10, t2 * 10, res_class == res_wino))
+          "Imporove classic multiplication of matrix: {2:f} sec\n"
+          "Are result matrix equivalent? {3}".format(t1 * 10, t2 * 10, t3 * 10, res_class == res_wino))
 
 
 if __name__ == '__main__':
