cd95fb12e57e0c3950753598291c9917e8e5c602
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-11-22 14:43:23 +0300

    Added report of lab3

--- Lab3. Sorting/cocktail sort.py
+++ Lab3. Sorting/cocktail sort.py
@@ -1,25 +1,45 @@
 import random
 import time
 
-N = 10000
+N = 10_000
 TIMES = 10
 
 
-def std_selection_sort(x):
-    for i in range(len(x)):
-        min_ind = i
-        for j in range(i, len(x)):
-            if x[min_ind] > x[j]:
-                min_ind = j
-        x[i], x[min_ind] = x[min_ind], x[i]
+def std_cocktail_sort (x):
+    left = 0
+    right = len(x)-1
+
+    while left <= right:
+        for i in range(left, right):
+            if x[i] > x[i+1]:
+                x[i], x[i+1] = x[i+1], x[i]
+        right = right - 1
+
+        for i in range(right, left, -1):
+            if x[i-1] > x[i]:
+                x[i-1], x[i] = x[i], x[i-1]
+        left = left + 1
 
     return x
 
 
-def opt_selection_sort(x):
-    for i, e in enumerate(x):
-        mn = min(range(i, len(x)), key=x.__getitem__)
-        x[i], x[mn] = x[mn], e
+def opt_cocktail_sort(x):
+    for i in range(len(x)//2):
+        swap = False
+        for j in range(1+i, len(x)-i):
+            if x[j] < x[j-1]:
+                x[j], x[j - 1] = x[j - 1], x[j]
+                swap = True
+        if not swap:
+            break
+        swap = False
+        for j in range(len(x)-i-1, i, -1):
+            if x[j] < x[j-1]:
+                x[j], x[j - 1] = x[j - 1], x[j]
+                swap = True
+        if not swap:
+            break
+
     return x
 
 
@@ -29,48 +49,49 @@ if __name__ == "__main__":
     c = []
     for i in range(N):
         a.append(i)
-        b.append(N - i)
+        b.append(N-i)
         c.append(random.randint(-N, N))
 
-    print("NO OPTIMIZED std_shell_sort(x)")
+    print("NO OPTIMIZED std_cocktail_sort(x)")
 
     print("\n...SORTING ARRAY...")
     st = time.time()
-    na = std_selection_sort(a)
+    na = std_cocktail_sort(a)
     end = time.time()
     print("\nTIME:", (end - st), "  sec")
 
     print("\n...REVERSE ARRAY...")
     st = time.time()
-    nb = std_selection_sort(b)
+    nb = std_cocktail_sort(b)
     end = time.time()
-    print("\nTIME:", (end - st), "  sec")
+    print( "\nTIME:", (end - st), "  sec")
 
     print("\n...RANDOM ARRAY...")
     st = time.time()
-    nc = std_selection_sort(c)
+    nc = std_cocktail_sort(c)
     end = time.time()
     print("\nTIME:", (end - st), "  sec")
 
-    print("\n\nOPTIMIZED opt_shell_sort(x)")
+    print("\n\nOPTIMIZED opt_cocktail_sort(x)")
 
     print("\n...SORTING ARRAY...")
     st = time.time()
-    na = opt_selection_sort(a)
+    na = opt_cocktail_sort(a)
     end = time.time()
     print("\nTIME:", (end - st), "  sec")
 
     print("\n...REVERSE ARRAY...")
     st = time.time()
-    nb = opt_selection_sort(b)
+    nb = opt_cocktail_sort(b)
     end = time.time()
     print("\nTIME:", (end - st), "  sec")
 
     print("\n...RANDOM ARRAY...")
     st = time.time()
-    nc = opt_selection_sort(c)
+    nc = opt_cocktail_sort(c)
     end = time.time()
     print("\nTIME:", (end - st), "  sec")
+
 
     # for i in range(N, 1100, 100):
     #     a = []
@@ -85,37 +106,37 @@ if __name__ == "__main__":
     #
     #     for j in range(TIMES):
     #         st = time.time()
-    #         std_selection_sort(a)
+    #         std_cocktail_sort(a)
     #         end = time.time()
     #
     #         sa += (end - st) * 1000
     #
     #         st = time.time()
-    #         std_selection_sort(b)
+    #         std_cocktail_sort(b)
     #         end = time.time()
     #
     #         sb += (end - st) * 1000
     #
     #         st = time.time()
-    #         std_selection_sort(c)
+    #         std_cocktail_sort(c)
     #         end = time.time()
     #
     #         sc += (end - st) * 1000
     #
     #         st = time.time()
-    #         opt_selection_sort(a)
+    #         opt_cocktail_sort(a)
     #         end = time.time()
     #
     #         oa += (end - st) * 1000
     #
     #         st = time.time()
-    #         opt_selection_sort(b)
+    #         opt_cocktail_sort(b)
     #         end = time.time()
     #
     #         ob += (end - st) * 1000
     #
     #         st = time.time()
-    #         opt_selection_sort(c)
+    #         opt_cocktail_sort(c)
     #         end = time.time()
     #
     #         oc += (end - st) * 1000
@@ -123,4 +144,3 @@ if __name__ == "__main__":
     #     print("\hline \n {0} & {1:.3f} | {2:.3f}  & {3:.3f} | {4:.3f} &"
     #           " {5:.3f} | {6:.3f}\\\\".format(i, sa/TIMES, oa/TIMES,
     #                                           sb/TIMES, ob/TIMES, sc/TIMES, oc/TIMES))
-    #
--- Lab3. Sorting/gnome sort.py
+++ Lab3. Sorting/gnome sort.py
@@ -1,7 +1,8 @@
 import random
 import time
 
-N = 10000
+N = 100
+TIMES = 10
 
 
 def std_gnome_sort(x):
@@ -30,56 +31,108 @@ def opt_gnome_sort(x):
 
 
 if __name__ == "__main__":
-    a = []
-    b = []
-    c = []
-    for i in range(N):
-        a.append(i)
-        b.append(N - i)
-        c.append(random.randint(-N, N))
+    # a = []
+    # b = []
+    # c = []
+    # for i in range(N):
+    #     a.append(i)
+    #     b.append(N - i)
+    #     c.append(random.randint(-N, N))
 
-    print("NO OPTIMIZED std_shell_sort(x)")
+    # print("NO OPTIMIZED std_shell_sort(x)")
+    #
+    # print("\n...SORTING ARRAY...")
+    # st = time.time()
+    # na = std_gnome_sort(a)
+    # end = time.time()
+    # print(na == sorted(a))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000), "  msec")
+    #
+    # print("\n...REVERSE ARRAY...")
+    # st = time.time()
+    # nb = std_gnome_sort(b)
+    # end = time.time()
+    # print(nb == sorted(b))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000), "  msec")
+    #
+    # print("\n...RANDOM ARRAY...")
+    # st = time.time()
+    # nc = std_gnome_sort(c)
+    # end = time.time()
+    # print(nc == sorted(c))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000), "  msec")
+    #
+    # print("\n\nOPTIMIZED opt_shell_sort(x)")
+    #
+    # print("\n...SORTING ARRAY...")
+    # st = time.time()
+    # na = opt_gnome_sort(a)
+    # end = time.time()
+    # print(na == sorted(a))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000),  " msec")
+    #
+    # print("\n...REVERSE ARRAY...")
+    # st = time.time()
+    # nb = opt_gnome_sort(b)
+    # end = time.time()
+    # print(nb == sorted(b))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000),  "  msec")
+    #
+    # print("\n...RANDOM ARRAY...")
+    # st = time.time()
+    # nc = opt_gnome_sort(c)
+    # end = time.time()
+    # print(nc == sorted(c))
+    # print("\nTIME: {0:.3f}".format((end - st) * 1000),  "  msec")
 
-    print("\n...SORTING ARRAY...")
-    st = time.time()
-    na = std_gnome_sort(a)
-    end = time.time()
-    print(na == sorted(a))
-    print("\nTIME:", (end - st), "  sec")
+    for i in range(N, 1100, 100):
+        a = []
+        b = []
+        c = []
+        for k in range(i):
+            a.append(k)
+            b.append(i - k)
+            c.append(random.randint(-i, i))
 
-    print("\n...REVERSE ARRAY...")
-    st = time.time()
-    nb = std_gnome_sort(b)
-    end = time.time()
-    print(nb == sorted(b))
-    print("\nTIME:", (end - st), "  sec")
+        sa, sb, sc, oa, ob, oc = 0, 0, 0, 0, 0, 0
 
-    print("\n...RANDOM ARRAY...")
-    st = time.time()
-    nc = std_gnome_sort(c)
-    end = time.time()
-    print(nc == sorted(c))
-    print("\nTIME:", (end - st), "  sec")
+        for j in range(TIMES):
+            st = time.time()
+            std_gnome_sort(a)
+            end = time.time()
 
-    print("\n\nOPTIMIZED opt_shell_sort(x)")
+            sa += (end - st) * 1000
 
-    print("\n...SORTING ARRAY...")
-    st = time.time()
-    na = opt_gnome_sort(a)
-    end = time.time()
-    print(na == sorted(a))
-    print("\nTIME:", (end - st), "  sec")
+            st = time.time()
+            std_gnome_sort(b)
+            end = time.time()
 
-    print("\n...REVERSE ARRAY...")
-    st = time.time()
-    nb = opt_gnome_sort(b)
-    end = time.time()
-    print(nb == sorted(b))
-    print("\nTIME:", (end - st), "  sec")
+            sb += (end - st) * 1000
 
-    print("\n...RANDOM ARRAY...")
-    st = time.time()
-    nc = opt_gnome_sort(c)
-    end = time.time()
-    print(nc == sorted(c))
-    print("\nTIME:", (end - st), "  sec")
+            st = time.time()
+            std_gnome_sort(c)
+            end = time.time()
+
+            sc += (end - st) * 1000
+
+            st = time.time()
+            opt_gnome_sort(a)
+            end = time.time()
+
+            oa += (end - st) * 1000
+
+            st = time.time()
+            opt_gnome_sort(b)
+            end = time.time()
+
+            ob += (end - st) * 1000
+
+            st = time.time()
+            opt_gnome_sort(c)
+            end = time.time()
+
+            oc += (end - st) * 1000
+
+        print("\hline \n {0} & {1:.3f} | {2:.3f}  & {3:.3f} | {4:.3f} &"
+              " {5:.3f} | {6:.3f}\\\\".format(i, sa/TIMES, oa/TIMES,
+                                              sb/TIMES, ob/TIMES, sc/TIMES, oc/TIMES))
