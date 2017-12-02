0529ce5eeae7f17565b3ca67e395a4320493c54b
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-12-03 00:54:47 +0300

    Added control graph and info history

--- Lab6/alg.py
+++ Lab6/alg.py
@@ -1,4 +1,47 @@
-def cohen_sutherland(bar, rect, win):
+def get_code(a, rect): 
+    code = [0, 0, 0, 0] 
+    if a[0] < rect[0]:
+        code[0] = 1  
+    if a[0] > rect[1]: 
+        code[1] = 1 
+    if a[1] < rect[2]:  
+        code[2] = 1  
+    if a[1] > rect[3]:  
+        code[3] = 1  
+
+    return code  
+
+def log_prod(code1, code2):  # (9.6)
+    p = 0  # (9.6.1)
+    for i in range(4):  # (9.6.2)
+        p += code1[i] & code2[i]  # (9.6.3)
+
+    return p  # (9.6.4)
+
+def is_visible(bar, rect): # (9)
+    """Видимость - 0 = невидимый
+                   1 = видимый
+                   2 = частично видимый"""
+    # вычисление кодов концевых точек отрезка
+    s1 = sum(get_code(bar[0], rect)) # (9.1)
+    s2 = sum(get_code(bar[1], rect)) # (9.2)
+
+    # предположим, что отрезок частично видим
+    vis = 2 # (9.3)
+
+    # проверка полной видимости отрезка
+    if not s1 and not s2: # (9.4)
+        vis = 1 # (9.5)
+    else:
+        # проверка тривиальной невидимости отрезка
+        l = log_prod(get_code(bar[0], rect), get_code(bar[1], rect)) # (9.6 == 9.6.4)
+        if l != 0: # (9.7)
+            vis = 0 # (9.8)
+
+    return vis  # (9.9)
+
+
+def cohen_sutherland(bar, rect): # (0)
     # инициализация флага
     flag = 1 # общего положения (1)
     t = 1 # (2)
@@ -14,33 +57,33 @@ def cohen_sutherland(bar, rect, win):
 
     # для каждой стороны окна
     for i in range(4): # (8)
-        vis = is_visible(bar, rect) # (9)
-        if vis == 1:  #
-            scene.addLine(bar[0][0], bar[0][1], bar[1][0], bar[1][1])  # (10)
-            return  # (11)
-        elif not vis:  # (12)
-            return  # (13)
+        vis = is_visible(bar, rect) # (9 == 9.9)
+        if vis == 1:  # (10)
+            scene.addLine(bar[0][0], bar[0][1], bar[1][0], bar[1][1])  # (11)
+            return  # (12)
+        elif not vis:  # (13)
+            return  # (14)
 
         # проверка пересечения отрезка и стороны окна
-        code1 = get_code(bar[0], rect)  # (14)
-        code2 = get_code(bar[1], rect)  # (15)
+        code1 = get_code(bar[0], rect)  # (15)
+        code2 = get_code(bar[1], rect)  # (16)
 
-        if code1[i] == code2[i]:  # (16)
-            continue  # (17)
+        if code1[i] == code2[i]:  # (17)
+            continue  # (18)
 
         # проверка нахождения Р1 вне окна; если Р1 внутри окна, то Р2 и Р1 поменять местами
-        if not code1[i]:  # (18)
-            bar[0], bar[1] = bar[1], bar[0]  # (19)
+        if not code1[i]:  # (19)
+            bar[0], bar[1] = bar[1], bar[0]  # (20)
 
         # поиск пересечений отрезка со сторонами окна
         # контроль вертикальности отрезка
-        if flag != -1:  # (20)
-            if i < 2:  # (21)
-                bar[0][1] = t * (rect[i] - bar[0][0]) + bar[0][1]  # (22)
-                bar[0][0] = rect[i]  # (23)
-                continue  # (24)
+        if flag != -1:  # (21)
+            if i < 2:  # (22)
+                bar[0][1] = t * (rect[i] - bar[0][0]) + bar[0][1]  # (23)
+                bar[0][0] = rect[i]  # (24)
+                continue  # (25)
             else:
-                bar[0][0] = (1 / t) * (rect[i] - bar[0][1]) + bar[0][0]  # (25)
+                bar[0][0] = (1 / t) * (rect[i] - bar[0][1]) + bar[0][0]  # (26)
 
-        bar[0][1] = rect[i]  # (26)
-    scene.addLine(bar[0][0], bar[0][1], bar[1][0], bar[1][1]) # (27)
+        bar[0][1] = rect[i]  # (27)
+    scene.addLine(bar[0][0], bar[0][1], bar[1][0], bar[1][1]) # (28)
